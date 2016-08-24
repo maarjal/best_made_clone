cf808ade476050b119b2d9aaeb7c5bb3adff0e5e
Maarja Laineste and Noah Yasskin <maarja+noah@>
2016-08-24 15:31:17 -0700

    calculate shpooing cart total

--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -2,8 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   items: [],
+  currentTotal: 0,
 
   add(item) {
     this.get('items').pushObject(item);
+    this.get('updateTotal')(this);
+  },
+
+  updateTotal: function(context) {
+    var total = 0;
+    context.get('items').forEach(function(item) {
+      total += parseInt(item.get('price'));
+    }.bind(total));
+    context.set('currentTotal', total);
+    console.log(total);
   }
 });
