e325a0b8ef6c203fa66dce4585f9f949942f8075
Maarja Laineste and Noah Yasskin <maarja+noah@>
2016-08-24 14:05:25 -0700

    display all products on all-products route

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('visit-us');
   this.route('product', {path: '/product/:product_id'});
+  this.route('all-products');
 });
 
 export default Router;
